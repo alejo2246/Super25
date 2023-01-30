84fb024ef0d7fab9f1f95c64a9b6c0a7136c9b16
Juan Arango <jp15912@outlook.com>
2023-01-30 18:07:56 -0500

    Opcion 2 y 3 agregada, codigo comentado

--- src/main/java/com/mycompany/supermercado_super_25/Supermercado_SUPER_25.java
+++ src/main/java/com/mycompany/supermercado_super_25/Supermercado_SUPER_25.java
@@ -3,8 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.supermercado_super_25; /// prueba push
+package com.mycompany.supermercado_super_25;
 
+import java.util.Calendar;
 /**
  *
  * @author USUARIO
@@ -17,6 +18,7 @@ public class Supermercado_SUPER_25 {
         /// Creacion de Variables para uso general
         String MainMenuOption = "";
         int Acceso = 0;
+        String UserName = "";
         Productos producto[] = new Productos[20]; /// Creacion de array de objetos con 20(MAX) posiciones a modo de
                                                   /// limitacion en arrays dinamicos en Java
         int SumaProductos = 0;
@@ -32,7 +34,7 @@ public class Supermercado_SUPER_25 {
             System.out.print("\n\t\tSUPER-25 LOGIN\nUsuario: ");
 
             /// Lectura de consola
-            String UserName = ScannerObject.nextLine();
+            UserName = ScannerObject.nextLine();
             System.out.print("Contrasena: ");
             String UserPassword = ScannerObject.nextLine();
 
@@ -51,6 +53,7 @@ public class Supermercado_SUPER_25 {
                 System.out.println("\nEl usuario o la contraseña no son validos, por favor intente de nuevo");
             }
         }
+
         while (!MainMenuOption.equals("5")) {
             /// Comunicacion con el usuario para el menu principal
             System.out.println(
@@ -102,35 +105,156 @@ public class Supermercado_SUPER_25 {
                 case "2": {
                     int MenuCuponesOpcion = 0;
                     System.out.println("\t\tBienvenido al registro de cupones");
+                    /// Comunicacion por consola
                     while (MenuCuponesOpcion != 2) {
                         int codigoExiste = 0;
                         System.out.println(
                                 "\nPor favor, seleccione una opcion:\n1) Agregar un cupon\n2) Volver al menu principal");
+                        /// Lectura de consola y conversion de String a entero
                         MenuCuponesOpcion = Integer.parseInt(ScannerObject.nextLine());
+
                         if (MenuCuponesOpcion == 1) {
-                            System.out.println("Codigo del cupon: ");
+                            System.out.print("Codigo del cupon: ");
                             String nombreCupon = ScannerObject.nextLine();
-                            System.out.println("Valor del cupon (0 - 100): ");
+                            System.out.print("Valor del cupon (0 - 100): ");
                             float descuentoCupon = Float.parseFloat(ScannerObject.nextLine());
+                            /// Prevencion de pre-existencia del cupon
                             for (int i = 0; i < sumaCupones; i++) {
                                 if (cupones[i].nombreCodigo.equals(nombreCupon))
                                     codigoExiste = 1;
                             }
-                            if (codigoExiste != 1) {
-                                cupones[sumaCupones] = new codigoDescuento(nombreCupon, descuentoCupon);
-                                sumaCupones++;
-                                System.out.println("El Cupon ha sido registrado exitosamente");
-                            } else {
+                            /// Validacion de errores
+                            if (codigoExiste == 1) {
                                 System.out.println("El cupon ya existe, intentelo de nuevo");
+                            } else if (nombreCupon.length() != 4) {
+                                System.out.println("El cupon debe tener exactamente 4 digitos");
+                            } else if (descuentoCupon < 0 || descuentoCupon > 100) {
+                                System.out.println(
+                                        "El cupon posee un descuento invalido, este debe ser un valor entre 0 y 100");
+                            } else {
+                                /// Agregar el cupon a la lista de descuentos existentes
+                                cupones[sumaCupones] = new codigoDescuento(nombreCupon, descuentoCupon);
+                                sumaCupones++;/// sumador para garantizar que siempre se guarden en la ultima posicion
+                                              /// de la lista
+                                System.out.println("El Cupon ha sido registrado exitosamente");
                             }
-
                         }
                     }
                     break;
                 }
                 case "3": {
-                    break;
+                    System.out.println(
+                            "\t\tBienvenido al menu de venta");
 
+                    /// Lectura y comunicacion por consola
+                    System.out.println("Por favor, registre los siguientes datos del cliente\nNombre: ");
+                    String nombreCliente = ScannerObject.nextLine();
+                    System.out.println("NIT (opcional): ");
+                    String nitCliente = ScannerObject.nextLine();
+
+                    /// El NIT se valida como diferente de vacio, en caso de que se provea. De lo
+                    /// contrario se clasifica al cliente como C/F
+                    if (nitCliente.equals("")) {
+                        System.out.println("Cliente registrado con documento C/F");
+                        nitCliente = "C/F";
+                    }
+                    System.out.println("Ha registrado a " + nombreCliente + " con documento " + nitCliente);
+                    System.out.println(
+                            "Seleccione que productos desea con su indice respectivo, al finalizar indique `Terminar`\nProductos Registrados:");
+
+                    /// Visualizacion de productos registrados
+                    for (int i = 0; i < producto.length; i++) {
+                        System.out.println(
+                                "" + (i + 1) + ") " + producto[i].nombreProducto + " Precio: $" + producto[i].precio);/// Se
+                                                                                                                      /// evita
+                                                                                                                      /// mostrar
+                                                                                                                      /// un
+                                                                                                                      /// indice
+                                                                                                                      /// 0
+                                                                                                                      /// mediante
+                                                                                                                      /// i+1
+                    }
+
+                    /// Seleccion de productos para vender hasta que se ingrese `Terminar`
+                    int completado = 0;
+                    float total = 0;
+                    int[] Vendidos = new int[20];
+                    while (completado != 1) {
+                        /// Comunicacion por consola
+                        String productoSeleccionado = ScannerObject.nextLine();
+
+                        /// Logica negada para evaluar si es distinto a `terminar` incluyendo el caso de
+                        /// mayusculas
+                        if (!(productoSeleccionado.equals("terminar")) && !(productoSeleccionado.equals("Terminar"))) {
+                            System.out.println("Cantidad: ");
+                            int cantidadProducto = Integer.parseInt(ScannerObject.nextLine());
+                            int prodSeleccionado = Integer.parseInt(productoSeleccionado);
+
+                            /// Eliminando el impacto de i+1 se actualiza la cantidad vendida del producto y
+                            /// la cantidad vendida para la sesion (cliente) actual
+                            producto[prodSeleccionado - 1].cantidadSesion = cantidadProducto;
+                            producto[prodSeleccionado
+                                    - 1].cantidadVendida = producto[prodSeleccionado - 1].cantidadVendida
+                                            + cantidadProducto;
+
+                            /// Se calcula el subtotal a medida que se itera
+                            total += producto[prodSeleccionado - 1].precio * cantidadProducto;
+
+                            /// Se registran los indices de los productos vendidos en caso que el usuario
+                            /// decida registrar nuevamente un mismo producto
+                            /// De esta forma se podra visualizar apropiadamente en la factura
+                            Vendidos[Vendidos.length - 1] = prodSeleccionado;
+                        } else
+                            completado = 1;
+                    }
+
+                    /// Comunicacion por consola para validar el posible descuento
+                    System.out.println("Codigo de descuento (opcional): ");
+                    String CodDescuento = ScannerObject.nextLine();
+                    float descuentoAplicado = 1;
+
+                    /// Si el descuento es diferente a un enter se entrara a buscar el nombre
+                    /// ingresado del cupon mediante la iteracion de la lista que contiene los
+                    /// cupones registrados
+                    if (!CodDescuento.equals("")) {
+                        int codDescuentoAux = Integer.parseInt(CodDescuento);
+                        for (int i = 0; i < cupones.length; i++) {
+                            if (CodDescuento.equals(cupones[i].nombreCodigo)) {
+                                /// Se prepara la multiplicacion del descuento al total
+                                descuentoAplicado = cupones[i].descuento / 100;
+                            }
+                        }
+                    }
+
+                    /// Facturacion
+                    /// Se utiliza la libreria Calendar para obtener la fecha
+                    Calendar calendar = Calendar.getInstance();
+
+                    /// Se hace un bosquejo de la factura con respuestas de consola dinamicas
+                    System.out.println("\t\t\tSUPER-25");
+                    System.out.println("\t\t" + calendar.getTime());
+                    System.out.println(
+                            "---------------------------------------------------------------------------------------");
+                    System.out.println("\tCajero:\t\t" + UserName);
+                    System.out.println("\tCliente:\t\t" + nombreCliente);
+                    System.out.println("\tNIT:\t\t" + nitCliente);
+                    System.out.println(
+                            "---------------------------------------------------------------------------------------");
+                    System.out.println("\tProducto  _ _ _ Precio/u _ _ _ Cantidad _ _ _ Total");
+                    for (int i = 0; i < Vendidos.length; i++) {
+                        System.out.println("\t" + producto[Vendidos[i]].nombreProducto + " _ _ _ "
+                                + producto[Vendidos[i]].precio + " " + producto[Vendidos[i]].cantidadVendida + " "
+                                + producto[Vendidos[i]].cantidadVendida * producto[Vendidos[i]].precio);
+                    }
+                    System.out.println(
+                            "---------------------------------------------------------------------------------------");
+                    System.out.println("\tSubtotal: \t\t" + total);
+                    System.out.println("\tDescuento: \t\t" + descuentoAplicado * 100 + "%");
+                    System.out.println(
+                            "---------------------------------------------------------------------------------------");
+                    System.out.println("\tTotal: \t\t" + total * descuentoAplicado);
+
+                    break;
                 }
                 case "4": {
                     break;
