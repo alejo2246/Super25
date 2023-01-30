8519036302ef9b6628a84d5ced670d2eef3346c3
alejo2246 <alejopadilla297@gmail.com>
2023-01-30 14:59:43 -0500

    .

--- src/main/java/com/mycompany/supermercado_super_25/Supermercado_SUPER_25.java
+++ src/main/java/com/mycompany/supermercado_super_25/Supermercado_SUPER_25.java
@@ -20,6 +20,8 @@ public class Supermercado_SUPER_25 {
         int Acceso = 0;
         Productos producto[] = new Productos[20]; /// Creacion de array de objetos con 20(MAX) posiciones a modo de limitacion en arrays dinamicos en Java
         int SumaProductos=0;
+        codigoDescuento cupones[]=new codigoDescuento[10];
+        int sumaCupones=0;
         /// Creacion de crdenciales
         String[] users = { "cajero_1", "cajero_2", "cajero_3" };
         String[] passwords = { "ipc_1", "ipc_2", "ipc_3" };
@@ -49,7 +51,7 @@ public class Supermercado_SUPER_25 {
                 System.out.println("\nEl usuario o la contraseña no son validos, por favor intente de nuevo");
             }
         }
-        while (!MainMenuOption.equals(5)) {
+        while (!MainMenuOption.equals("5")) {
             /// Comunicacion con el usuario para el menu principal
             System.out.println(
                     "\nBienvenido al sistema Cashier de SUPER-25\n\n\t\tMenu Principal\nPor favor seleccione una de las siguientes opciones:");
@@ -96,6 +98,31 @@ public class Supermercado_SUPER_25 {
                     break;
                 }
                 case "2": {
+                    int MenuCuponesOpcion=0;
+                    System.out.println("\t\tBienvenido al registro de cupones");
+                    while (MenuCuponesOpcion!=2){
+                        int codigoExiste=0;
+                        System.out.println("\nPor favor, seleccione una opcion:\n1) Agregar un cupon\n2) Volver al menu principal");
+                        MenuCuponesOpcion=Integer.parseInt(ScannerObject.nextLine());
+                        if(MenuCuponesOpcion==1){
+                            System.out.println("Codigo del cupon: ");
+                            String nombreCupon=ScannerObject.nextLine();
+                            System.out.println("Valor del cupon (0 - 100): ");
+                            float descuentoCupon=Float.parseFloat(ScannerObject.nextLine());
+                            for (int i=0; i<sumaCupones;i++){
+                                if(cupones[i].nombreCodigo.equals(nombreCupon)) 
+                                    codigoExiste=1;
+                            }
+                            if(codigoExiste!=1){
+                                cupones[sumaCupones]= new codigoDescuento(nombreCupon,descuentoCupon);
+                                sumaCupones++;
+                                System.out.println("El Cupon ha sido registrado exitosamente");                                
+                            }else{
+                                System.out.println("El cupon ya existe, intentelo de nuevo");
+                            }
+
+                        }
+                    }                    
                     break;
                 }
                 case "3": {
@@ -106,6 +133,7 @@ public class Supermercado_SUPER_25 {
                     break;
                 }
                 case "5": {
+                    MainMenuOption = "5";
                     break;
                 }
                 default: {
